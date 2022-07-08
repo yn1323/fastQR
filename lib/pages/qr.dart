13fecd40c41c7ce07252a685903279173c47bfbd
yn1323 <yn1323@gmail.com>
2022-07-08 23:10:02 +0900

    fix: libs

---
+++ lib/pages/qr.dart
@@ -0,0 +1,48 @@
+
+import 'package:fast_qr/imports.dart';
+
+class Qr extends StatefulWidget {
+  const Qr({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<Qr> createState() => _QrState();
+}
+
+class _QrState extends State<Qr> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many tiaames:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
